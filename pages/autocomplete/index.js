489ee51f9a833686e018287296dc625e78818e4e
Sebastian Contreras <sebastian.contreras@algolia.com>
2022-10-26 09:16:36 -0500

    Fiing Highlihting ready for Demo.

--- pages/autocomplete/index.js
+++ pages/autocomplete/index.js
@@ -53,7 +53,7 @@ function QuerySuggestionItem({ item }) {
             <path d="M16.041 15.856c-0.034 0.026-0.067 0.055-0.099 0.087s-0.060 0.064-0.087 0.099c-1.258 1.213-2.969 1.958-4.855 1.958-1.933 0-3.682-0.782-4.95-2.050s-2.050-3.017-2.050-4.95 0.782-3.682 2.050-4.95 3.017-2.050 4.95-2.050 3.682 0.782 4.95 2.050 2.050 3.017 2.050 4.95c0 1.886-0.745 3.597-1.959 4.856zM21.707 20.293l-3.675-3.675c1.231-1.54 1.968-3.493 1.968-5.618 0-2.485-1.008-4.736-2.636-6.364s-3.879-2.636-6.364-2.636-4.736 1.008-6.364 2.636-2.636 3.879-2.636 6.364 1.008 4.736 2.636 6.364 3.879 2.636 6.364 2.636c2.125 0 4.078-0.737 5.618-1.968l3.675 3.675c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414z" />
           </svg>
         </div>
-        <Highlight hit={item} attribute='query'/>
+        <Highlight hit={item} attribute='query' />
       </div>
       <div className="aa-ItemActions">
         <button
@@ -103,8 +103,8 @@ function Autocomplete() {
                       query,
                       params: {
                         hitsPerPage: 4,
-                        highlightPreTag: '<mark>',
-                        highlightPostTag: '</mark>',
+                        // highlightPreTag: '<mark>',
+                        // highlightPostTag: '</mark>',
                       },
                     },
                   ],
@@ -129,12 +129,8 @@ function Autocomplete() {
         {autocompleteState.isOpen &&
           autocompleteState.collections.map((collection, index) => {
             const { source, items } = collection;
-
+            // Rendering of Query Suggestions
             if (collection.source.sourceId === 'querySuggestionsPlugin') {
-              console.log({
-                ...autocomplete.getItemProps({item: items[0], source})
-                })
-
               return (
                 <div key={`source-${index}`} className="aa-Source">
                   <ul className="aa-List" {...autocomplete.getListProps()}>
@@ -153,7 +149,7 @@ function Autocomplete() {
                 </div>
               )
             }
-
+            // Rendering of regular items
             return (
               <div key={`source-${index}`} className="aa-Source">
                 {items.length > 0 && (
@@ -167,7 +163,11 @@ function Autocomplete() {
                           source,
                         })}
                       >
-                        {item.name ? item.name : item.query}
+                        <div className="aa-ItemWrapper">
+                          <div className="aa-ItemContent">
+                            <Highlight hit={item} attribute='name' />
+                          </div>
+                        </div>
                       </li>
                     ))}
                   </ul>
