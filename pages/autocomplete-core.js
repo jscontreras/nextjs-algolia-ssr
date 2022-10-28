d9d8b16d1867cf6a4cf231543160f079edbe1801
Sebastian Contreras <sebastian.contreras@algolia.com>
2022-10-28 09:32:09 -0500

    Removing unnecesary imports.

--- pages/autocomplete-core.js
+++ pages/autocomplete-core.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import { createAutocomplete } from '@algolia/autocomplete-core';
 import { createElement, Fragment } from 'react';
