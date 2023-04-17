696b28734b65b01fb0377f70416467b9f60d833b
Sebastian Contreras <sebastian.contreras@algolia.com>
2023-04-17 07:09:41 -0500

    Fixing dark color link.

--- pages/index.js
+++ pages/index.js
@@ -74,6 +74,7 @@ export default function Home() {
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
+          className={styles.powerLink}
         >
           Powered by{' '}
           <span className={styles.logo}>
